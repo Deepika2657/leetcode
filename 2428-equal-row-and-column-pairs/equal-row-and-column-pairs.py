acee7df2789a68a5244d1640fa9fc7daf9b62303
DEEPIKA KASTALA <deepikakastala26@gmail.com>
2026-06-18 15:19:58 +0530

    Time: 1643 ms (10.59%) | Memory: 15.6 MB (74.58%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -9,10 +9,12 @@ class Solution(object):
 
         for i in range(n):
             for j in range(n):
+                match = True
                 for k in range(n):
                     if grid[i][k] != grid[k][j]:
+                        match = False
                         break
-                else:
-                    pairs += 1
+                if match:
+                   pairs += 1
 
         return pairs 
