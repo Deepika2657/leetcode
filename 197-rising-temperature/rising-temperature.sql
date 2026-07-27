3442f4c18d605af736daf7c438cc16c235fa56d0
DEEPIKA KASTALA <deepikakastala26@gmail.com>
2026-07-27 12:34:24 +0530

    Time: 504 ms (52.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+#select id from Weather order by temperature desc limit 2;
+SELECT w1.id 
+FROM Weather w1
+JOIN Weather w2 
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
